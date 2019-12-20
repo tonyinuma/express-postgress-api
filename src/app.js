f24362dd5f2a1c393913ace3a59cbd948629b678
tonyinuma <tony.inuma@gmail.com>
2019-12-20 14:18:31 -0500

    Feat: Configuration routes and initialization server

--- src/app.js
+++ src/app.js
@@ -2,13 +2,20 @@
 import express, { json } from 'express';
 import morgan from 'morgan';
 
-const app = express();
+//Importing routes
+import projectRoutes from './routes/projects';
+import taskRoutes from './routes/tasks';
 
+//Initialization
+const app = express();
 
 //Middlewares
 app.use(morgan('dev'));
 app.use(json());
 
+// routes
+app.use('/api/projects',projectRoutes);
+app.use('/api/tasks',taskRoutes);
 
 
 
